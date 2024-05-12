134b019992b6f49e8ec88916910c17b9b19b6327
angelcruz07 <angelcruzsanchez126@gmail.com>
2024-05-12 16:32:21 -0600

    feat: add Response factory

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -12,28 +12,35 @@ class Response {
         return $this -> statusCode;
     }
 
-    public function setStatus(int $statusCode): void {
+    public function setStatus(int $statusCode): self {
         $this -> statusCode = $statusCode;
+        return $this;
     }
 
     public function headers(): array {
         return $this->headers;
     }
 
-    public function setHeader( string $header, string $value) {
+    public function setHeader( string $header, string $value): self {
         $this -> headers[strtolower($header)] = $value;
+        return $this;
     }
 
     public function removeHeader(string $header) {
         unset($this -> headers[strtolower($header)]);
+    }
+    public function setContentType(string $value): self{
+        $this -> setHeader('Content-Type', $value);
+        return $this;
     }
 
     public function content(): ?string {
         return $this -> content;
     }
 
-    public function setContent(string $content): void {
+    public function setContent(string $content): self {
         $this -> content = $content;
+        return $this;
     }
 
     public function prepare(){
@@ -45,4 +52,22 @@ class Response {
         }
     }
 
+    // Factory methods
+    public static function json(array $data): self {
+        return  (new self())
+        ->setContentType('application/json')
+        ->setContent(json_encode($data));
+    }
+
+    public static function text(string $text): self {
+        return  (new self())
+        ->setContentType('text/plain')
+        ->setContent($text);
+    }
+
+    public static function redirect(string $uri):self{
+        return (new self())
+        ->setStatus(302)
+        ->setHeader('Location', $uri);
+    }
 }
